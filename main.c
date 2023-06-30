fc9609b6dac76bfc7ed9072330dc7fd0a0163b31
ishant9805 <ishantkumar761@gmail.com>
2023-06-30 17:02:14 +0530

    coded the decision block

--- main.c
+++ main.c
@@ -2,7 +2,20 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello, User!");
+    char* cnf;
+    int todo;
+    printf("Hello, User!\nDo you want to use this calculator?(Y/N) ");
+    scanf('%c', &cnf);
+    if (*cnf == 'y' || *cnf == 'Y')
+    {
+        printf("What do you want to do?\n");
+        printf("if you want to add enter 1\n");
+        printf("if you want to subtract enter 2\n");
+        printf("if you want to multiply enter 3\n");
+        printf("if you want to divide enter 4\n");
+        scanf("Enter: %d", &todo);
+
+    }
     
     return 0;
 }
